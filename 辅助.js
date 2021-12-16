799d1f46965acea2027fa3c72df2cdd202306989
Ayasakimanaka <dd15162238220@163.com>
2021-12-16 09:44:06 -0500

    work

---
+++ 辅助.js
@@ -0,0 +1,5 @@
+function show() {
+        var a1 = document.getElementById("end");
+        var give = document.getElementById("give")
+        give.innerHTML = "Welcome," + a1.value + '.'  + "Before start your travel, I'll tell something about XuZhou"
+      }
